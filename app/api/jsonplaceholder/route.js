ae5bc2a29b33eabe1463f3a8d663c13a9033822e
flybear <goodgood0331@gmail.com>
2023-08-17 16:05:12 +0800

    change port

--- app/api/jsonplaceholder/route.js
+++
@@ -1,8 +0,0 @@
-// import { NextResponse } from "next/server";
-// import axios from "axios";
-// const url = "https://jsonplaceholder.typicode.com/posts?_limit=100";
-// export const GET = async () => {
-//   const res = await axios.get(url);
-//   const data = await res.data;
-//   return new NextResponse(JSON.stringify(data));
-// };
