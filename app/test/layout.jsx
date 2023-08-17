d308aec1e7e03bf1c0fecde354f1c41d3f33edbb
flybear <goodgood0331@gmail.com>
2023-08-17 17:26:01 +0800

    del page

--- app/test/layout.jsx
+++
@@ -1,8 +0,0 @@
-export default function RootLayout({ children, menu }) {
-  return (
-    <div className="bg-emerald-400 w-50 h-96">
-      <div className="w-full">{children}</div>
-      <div className="w-full">{menu}</div>
-    </div>
-  );
-}
--- components/child-component/editLightBox.jsx
+++ components/child-component/editLightBox.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import "../../styles/turntable.scss";
 export default function EditLightBox({
