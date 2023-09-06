afcfb2904df4e408d5b109d032d1e7f05beba97b
flybear <goodgood0331@gmail.com>
2023-09-06 11:37:51 +0800

    cmd typedJS

--- components/banner.jsx
+++ components/banner.jsx
@@ -7,10 +7,14 @@ export default function Banner() {
   useEffect(() => {
     const typed = new Typed(typeRef.current, {
       strings: [
-        "<span class='yellow'>const</span> <span class='blue'>newArray</span> <span>=</span> <span class='yellow'>[</span> <span class='orange'>'學React'</span> <span class='yellow'>]</span>",
+        "<span class='yellow'>const</span> <span class='blue'>todoList</span> <span>=</span> <span class='yellow'>[</span> <span class='orange'>'學React'</span> <span>,</span> <span class='orange'>'寫CSS'</span> <span class='yellow'>]<span/> <br> <span class='blue'>console</span> <span class='yellow'>.log</span> <span class='yellow'>(</span> <span class='blue'>todoList</span> <span class='yellow'>)</span>",
       ],
-      typeSpeed: 100,
+      typeSpeed: 20,
     });
+
+    setTimeout(() => {
+      console.log(["學React", "寫CSS"]);
+    }, 4000);
 
     return () => {
       typed.destroy();
@@ -21,7 +25,44 @@ export default function Banner() {
       <div class="w-full h-screen">
         {/* <div class="banner-bg"> */}
         <div class="overlayer">
-          <div class="codeArea" ref={typeRef}></div>
+          <div className="pd">
+            <div className="mac-btn">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="54"
+                height="14"
+                viewBox="0 0 54 14"
+              >
+                <g fill="none" fill-rule="evenodd" transform="">
+                  <circle
+                    cx="6"
+                    cy="6"
+                    r="6"
+                    fill="#FF5F56"
+                    stroke="#E0443E"
+                    stroke-width=".5"
+                  ></circle>
+                  <circle
+                    cx="26"
+                    cy="6"
+                    r="6"
+                    fill="#FFBD2E"
+                    stroke="#DEA123"
+                    stroke-width=".5"
+                  ></circle>
+                  <circle
+                    cx="46"
+                    cy="6"
+                    r="6"
+                    fill="#27C93F"
+                    stroke="#1AAB29"
+                    stroke-width=".5"
+                  ></circle>
+                </g>
+              </svg>
+            </div>
+            <div class="codeArea" ref={typeRef}></div>
+          </div>
           {/* </div> */}
         </div>
       </div>
